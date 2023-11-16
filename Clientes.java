8129c1e23580bfc85b765dfe7057459566d2c776
Marllonlp <marlonmota2019@gmail.com>
2023-11-16 07:50:05 -0300

    projeto

--- Clientes.java
+++ Clientes.java
@@ -2,6 +2,6 @@ public class Clientes {
     int id, data, hora, adultos, criancas;
     String email;
     String senha;
-
+    int senhaAdmim = 1234;
 
 }
--- Projeto_Guia_Turistico.java
+++ Projeto_Guia_Turistico.java
@@ -1,96 +1,48 @@
 import java.io.*;
 import java.util.Scanner;
-
-
 public class Projeto_Guia_Turistico {
     public static void main(String[] args) {
-
-        String path = "clientes/";
-        int id = 0;
-        id = inicializar(path);
-        Scanner sc = new Scanner(System.in);
-        Clientes cl = new Clientes();
-
+        Scanner ler = new Scanner(System.in);
         int opcao = 0;
-        while (opcao != 6) {
-            menu();
-            opcao = sc.nextInt();
-
-            switch (opcao) {
-                case 1:
-                    if (cadastrarUsuario(id, path)) {
-                        id++;
-                        gravarId(id);
-                    }
-                    ;
-                    break;
-                case 2:
-                    id = 0;
-                    reservaPasseio(id);
-                    break;
-                case 3:
-                    verificarReserva();
-                    break;
-                case 4:
-                    cancelarReserva();
-                    break;
-                case 5:
-                    pacotesDisponivel();
-                    break;
-                case 6:
-                    System.out.println("Saindo...");
-                    break;
-
-                default:
-                    System.out.println(" Opção Invalida! ");
-            }
+        menu();
+        opcao = ler.nextInt();
+        switch (opcao) {
+            case 1:
+                menuUsuario();
+                break;
+            case 2:
+                validarAdmin();
+                break;
+            case 3:
+                System.out.println("Encerrando...");
+                break;
+            default:
+                // Caso padrão: Opção inválida
+                System.out.println(" Opção Inválida! ");
         }
-
-
     }
-
-    private static void menu() {
-
-        System.out.println("\n|----------------------|");
-        System.out.println("  Cavernas do Peruaçu  ");
-        System.out.println("|----------------------|\n");
-        System.out.println(" 1 - Cadastrar Usuario ");
-        System.out.println(" 2 - Fazer Reserva ");
-        System.out.println(" 3 - Verificar Reserva ");
-        System.out.println(" 4 - Cancelar Reserva ");
-        System.out.println(" 5 - Pacotes Disponiveis ");
-
-        System.out.println(" 6 - Sair ");
-        System.out.print("\n Opção: ");
-
-    }
-
 
     private static boolean cadastrarUsuario(int id, String path) {
         Scanner sc = new Scanner(System.in);
         Clientes cl = new Clientes();
 
-        System.out.println("|-----------|");
-        System.out.println("  Cadastrar ");
-        System.out.println("|-----------|\n");
-
-
-        System.out.println("Email: ");
+        System.out.println("CADASTRAR: ");
+        System.out.println("CPF: ");
         cl.email = sc.nextLine();
-        System.out.println("Senha: ");
+        System.out.println("SENHA: ");
         cl.senha = sc.nextLine();
         cl.id = id;
         try {
             gravarCliente(cl, path);
         } catch (FileNotFoundException e) {
-            System.out.println("Não foi possivel cadastrar: ");
+            System.out.println("Não foi possível cadastrar: ");
             e.printStackTrace();
             return false;
         }
         return true;
-
     }
 
+    // Método para gravar as informações do cliente em um arquivo
     private static void gravarCliente(Clientes cl, String path) throws FileNotFoundException {
         PrintWriter pw = new PrintWriter(path + cl.id + ".txt");
         pw.println(cl.id);
@@ -100,7 +52,7 @@ public class Projeto_Guia_Turistico {
         pw.close();
     }
 
-
+    // Método para gravar o id atual em um arquivo
     private static void gravarId(int id) {
         PrintWriter pw;
         try {
@@ -109,32 +61,33 @@ public class Projeto_Guia_Turistico {
             pw.flush();
             pw.close();
         } catch (FileNotFoundException e) {
-            // TODO Auto-generated catch block
             e.printStackTrace();
         }
     }
 
+    // Método para inicializar o programa e ler o id inicial
     private static int inicializar(String path) {
         int id = 0;
         File dir = new File(path);
-        if (!dir.exists()) {            //Verifica se o diretório contendo contatos ja existe
-            dir.mkdir();                //cria se não existir
+        if (!dir.exists()) {
+            dir.mkdir();
         }
         File arquivo = new File("id.txt");
-        if (!arquivo.exists()) {        //verifica se o arquivo de id já existe
+        if (!arquivo.exists()) {
             try {
-                arquivo.createNewFile(); //cria se não existir
+                arquivo.createNewFile();
             } catch (IOException e) {
                 System.out.println("Não foi possível criar o ID");
                 e.printStackTrace();
             }
-            gravarId(0);                //grava o id=0 para inicializar a agenda
-        } else {                        //caso o arquivo de id já exista,
-            id = lerId();                //é feita a leitura
+            gravarId(0);
+        } else {
+            id = lerId();
         }
         return id;
     }
 
+    // Método para ler o id de um arquivo
     private static int lerId() {
         BufferedReader bf;
         int id = 0;
@@ -149,25 +102,29 @@ public class Projeto_Guia_Turistico {
         return id;
     }
 
+    // Método para verificar reservas
     private static void verificarReserva() {
         System.out.println("Sucesso");
     }
 
+    // Método para cancelar reservas
     private static void cancelarReserva() {
         System.out.println("Sucesso");
     }
 
+    // Método para exibir pacotes disponíveis
     private static void pacotesDisponivel() {
         System.out.println("Sucesso");
     }
 
-    private static boolean reservaPasseio(int id) {
+    // Método para lidar com reservas de cavernas
+    private static boolean reservaCaverna(int id) {
         Scanner sc = new Scanner(System.in);
         Clientes c = new Clientes();
         System.out.println("|-------------------|");
         System.out.println(" Reservando Passeio ");
         System.out.println("|-------------------|\n");
-        // Colocar data e hora
+        // Inserir data e hora
         System.out.print("Adultos: ");
         c.adultos = sc.nextInt();
         System.out.print("Crianças: ");
@@ -175,5 +132,125 @@ public class Projeto_Guia_Turistico {
         c.id = id;
 
         return false;
+    }
+
+    private static void menu() {
+        System.out.println("\n|----------------------|");
+        System.out.println("  Cavernas do Peruaçu  ");
+        System.out.println("|----------------------|");
+        System.out.println(" Entar como: ");
+        System.out.println(" 1 - Usuario");
+        System.out.println(" 2 - Administrador");
+        System.out.println(" 3 - Encerrar");
+        System.out.print("Opção:");
+    }
+
+    private static void menuUsuario() {
+        String path = "clientes/";
+        int id = 0;
+        id = inicializar(path);
+        Scanner ler = new Scanner(System.in);
+        Clientes cl = new Clientes();
+        int opcao = 0;
+
+        while (opcao != 5) {
+            // Exibindo o menu
+            System.out.println("\n|----------------------|");
+            System.out.println("  Cavernas do Peruaçu  ");
+            System.out.println("|----------------------|\n");
+            System.out.println(" 1 - Cadastrar Usuário ");
+            System.out.println(" 2 - Fazer Reserva ");
+            System.out.println(" 3 - Verificar Reserva ");
+            System.out.println(" 4 - Cancelar Reserva ");
+            System.out.println(" 5 - Sair ");
+            System.out.print("\n Opção: ");
+            opcao = ler.nextInt();
+
+            switch (opcao) {
+                case 1:
+                    // Opção 1: Cadastrar um novo usuário
+                    if (cadastrarUsuario(id, path)) {
+                        id++;
+                        gravarId(id);
+                    }
+                    break;
+                case 2:
+                    // Opção 2: Reservar uma caverna
+                    id = 0;
+                    reservaCaverna(id);
+                    break;
+                case 3:
+                    // Opção 3: Verificar reserva
+                    verificarReserva();
+                    break;
+                case 4:
+                    // Opção 4: Cancelar reserva
+                    cancelarReserva();
+                    break;
+                case 5:
+                    // Opção 6: Sair do programa
+                    System.out.println("Saindo...");
+                    break;
+                default:
+                    // Caso padrão: Opção inválida
+                    System.out.println(" Opção Inválida! ");
+            }
+        }
+    }
+
+    private static void validarAdmin() {
+        Scanner ler = new Scanner(System.in);
+        Administrador senhaAdm = new Administrador();
+        System.out.println("Infome a senha do Administrado");
+        int senha = ler.nextInt();
+
+        if (senha == senhaAdm.senhaAdmim) {
+            menuAdmin();
+        } else {
+            System.out.println("Senha incorreta");
+        }
+    }
+
+    private static void menuAdmin() {
+        String path = "clientes/";
+        int id = 0;
+        id = inicializar(path);
+        Scanner ler = new Scanner(System.in);
+        int opcao = 0;
+        while (opcao != 4) {
+            // Exibindo o menu
+            System.out.println("\n|----------------------|");
+            System.out.println("  Cavernas do Peruaçu  ");
+            System.out.println("|----------------------|\n");
+            System.out.println(" 1 - Fazer Reserva para Usuario ");
+            System.out.println(" 2 - Verificar Reserva ");
+            System.out.println(" 3 - Confirmar Reserva ");
+            System.out.println(" 4 - Sair ");
+            System.out.print("\n Opção: ");
+            opcao = ler.nextInt();
+
+            switch (opcao) {
+                case 1:
+                    id = 0;
+                    reservaCaverna(id);
+                    break;
+                case 2:
+                    verificarReserva();
+                    break;
+                case 3:
+                    confimarReserva();
+                    break;
+                case 4:
+                    System.out.println("Saindo...");
+                    break;
+                default:
+                    // Caso padrão: Opção inválida
+                    System.out.println(" Opção Inválida! ");
+                    break;
+            }
+        }
+    }
+    private static void confimarReserva() {
+
     }
 }
